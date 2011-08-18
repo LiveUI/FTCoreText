6e46ff1375bbea9f84d88f8b3bc7ac2bf7548e37
Francesco Frison <francesco@fuerteint.com>
2011-08-18 14:41:00 +0100

    init

---
+++ FTCoreText/FTCoreTextAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FTCoreTextAppDelegate.h
+//  FTCoreText
+//
+//  Created by Fuerte International on 18/08/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTCoreTextAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ FTCoreText/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FTCoreText
+//
+//  Created by Fuerte International on 18/08/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
